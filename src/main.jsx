147bb3a26e0422cb1c3a943acb7e4a45bd5602c8
loveOneBaby <1344899122@qq.com>
2026-07-31 21:20:25 +0800

    Release v0.2.8

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import { ProductPage } from "./ProductPage.jsx";
+import { getBridge } from "./services/desktopBridge.js";
 import "./styles.css";
 
-const showApp = window.location.hash === "#app" || new URLSearchParams(window.location.search).has("app");
+const isDesktop = Boolean(getBridge());
+const showApp =
+  isDesktop ||
+  window.location.hash === "#app" ||
+  new URLSearchParams(window.location.search).has("app");
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
